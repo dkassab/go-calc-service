2ab962c355f579f9f3099a25683bed38301540c9
Dima Kassab <dkassab@dkassab-mac.local>
2017-03-23 14:52:46 -0400

    adding the server-rproxy

---
+++ server-rproxy/server-rproxy.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	pb "github.com/dkassab/go-calc-service/calc"
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"net/http"
+)
+
+var (
+	addEndpoint = flag.String("add_endpoint", "server:33333", "endpoint of addService")
+)
+
+func RunEndPoint(address string, opts ...runtime.ServeMuxOption) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux(opts...)
+	dialOpts := []grpc.DialOption{grpc.WithInsecure()}
+	err := pb.RegisterCalculatorHandlerFromEndpoint(ctx, mux, *addEndpoint, dialOpts)
+	if err != nil {
+		return err
+	}
+
+	http.ListenAndServe(address, mux)
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := RunEndPoint(":8080"); err != nil {
+		glog.Fatal(err)
+	}
+}
